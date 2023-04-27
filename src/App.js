e273300c89242a73a8cc7385ef0766058f601504
Taehyung Kwon <katalogin88@gmail.com>
2023-04-27 22:06:28 +0100

    Make get livestream creatiton response

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import VideoPlayer from "./components/videoplayer";
-import { axios } from "axios";
-
-function handleSubmit(event) {
-  event.preventDefault();
-  console.log("Submitted");
-}
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    console.log("Submitted");
+
+    axios.post("http://localhost:8000/create-live-stream").then((response) => {
+      setData(response.data);
+    });
+  };
+
   return (
     <div className="App">
       <h1>Parrotias Livestreaming</h1>
       <VideoPlayer playbackId="" />
-      <form
-        action="http://localhost:8000/create-live-stream"
-        method="POST"
-        onSubmit={handleSubmit}
-      >
+      <form action="#" method="POST" onSubmit={handleSubmit}>
         <button type="submit">Create Live Stream</button>
       </form>
+      <div>{JSON.stringify(data)}</div>
     </div>
   );
 }
