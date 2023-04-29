7146df1cb81e85901b88e6412c0d9ee5d62fbab5
Taehyung Kwon <katalogin88@gmail.com>
2023-04-29 12:22:35 +0100

    Add set live stream button to separate creating and playback live stream

--- src/App.js
+++ src/App.js
@@ -6,25 +6,46 @@ import { useState } from "react";
 
 function App() {
   const [data, setData] = useState({});
+  const [playbackId, setPlaybackId] = useState("");
+  const [streamKey, setStreamKey] = useState("");
 
+  // Handle Create Live Stream button to create a live stream
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     console.log("Submitted");
 
-    axios.post("http://localhost:8000/create-live-stream").then((response) => {
-      setData(response.data);
-    });
+    await axios
+      .post("http://localhost:8000/create-live-stream")
+      .then((response) => {
+        setData(response.data);
+      });
   };
+
+  // Handle Set Live Stream button to set a playback Id and stream key.
+  // Playback ID is used to play the live stream in VideoPlayer component.
+  const handleSet = async (event) => {
+    event.preventDefault();
+
+    console.log("Set");
+
+    setPlaybackId(data["data"]["playback_id"]);
+    setStreamKey(data["data"]["stream_key"]);
+  };
+
+  console.log("playbackId: ", playbackId);
 
   return (
     <div className="App">
       <h1>Parrotias Livestreaming</h1>
-      <VideoPlayer playbackId="" />
+      <VideoPlayer playbackId={playbackId} videoId="" />
       <form action="#" method="POST" onSubmit={handleSubmit}>
         <button type="submit">Create Live Stream</button>
       </form>
       <div>{JSON.stringify(data)}</div>
+      <form onSubmit={handleSet}>
+        <button type="submit">Set Live Stream</button>
+      </form>
     </div>
   );
 }
--- src/components/videoplayer.jsx
+++ src/components/videoplayer.jsx
@@ -10,7 +10,7 @@ export default function VideoPlayer(props) {
       streamType="live"
       playbackId={playbackId}
       metadata={{
-        video_id: videoId,
+        video_id: "video-id-54321",
         video_title: "Test video title",
         viewer_user_id: "user-id-007",
       }}
