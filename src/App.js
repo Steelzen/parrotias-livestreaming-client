7ba6025ec856f61580de37054210170e667c3f71
Taehyung Kwon <katalogin88@gmail.com>
2023-05-01 20:42:13 +0100

    structure pages and add streaming video function

--- src/App.js
+++ src/App.js
@@ -1,51 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
-import axios from "axios";
-import VideoPlayer from "./components/videoplayer";
-import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  redirect,
+  Route,
+  Routes,
+} from "react-router-dom";
+import LiveStream from "./pages/livestream_mux";
+import PlayMyVideo from "./pages/playMyVideo";
+import MainPage from "./pages/mainpage";
 
 function App() {
-  const [data, setData] = useState({});
-  const [playbackId, setPlaybackId] = useState("");
-  const [streamKey, setStreamKey] = useState("");
-
-  // Handle Create Live Stream button to create a live stream
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    console.log("Submitted");
-
-    await axios
-      .post("http://localhost:8000/create-live-stream")
-      .then((response) => {
-        setData(response.data);
-      });
-  };
-
-  // Handle Set Live Stream button to set a playback Id and stream key.
-  // Playback ID is used to play the live stream in VideoPlayer component.
-  const handleSet = async (event) => {
-    event.preventDefault();
-
-    console.log("Set");
-
-    setPlaybackId(data["data"]["playback_id"]);
-    setStreamKey(data["data"]["stream_key"]);
-  };
-
-  console.log("playbackId: ", playbackId);
-
   return (
     <div className="App">
-      <h1>Parrotias Livestreaming</h1>
-      <VideoPlayer playbackId={playbackId} videoId="" />
-      <form action="#" method="POST" onSubmit={handleSubmit}>
-        <button type="submit">Create Live Stream</button>
-      </form>
-      <div>{JSON.stringify(data)}</div>
-      <form onSubmit={handleSet}>
-        <button type="submit">Set Live Stream</button>
-      </form>
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+        <Route path="/livestream" element={<LiveStream />} />
+        <Route path="/myvideo" element={<PlayMyVideo />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/playMyVideo.jsx
@@ -0,0 +1,12 @@
+import MyVideo from "../components/myVideo";
+
+const PlayMyVideo = () => {
+  return (
+    <div>
+      <h1>Parrotias Video Streaming</h1>
+      <MyVideo />
+    </div>
+  );
+};
+
+export default PlayMyVideo;
